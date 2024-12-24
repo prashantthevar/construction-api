0137d222f44a04f018b03e08167c65e4eec99b15
Prashant Thevar <prashant.thevar@gmail.com>
2024-12-24 22:58:43 +0530

    add cors

--- Program.cs
+++ Program.cs
@@ -52,7 +52,19 @@ builder.Services.AddSingleton<RoleSeeder>();
 builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Enable Swagger UI in development and production environments
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
